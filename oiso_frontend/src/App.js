a82b51ab4fe1f42406624c696e4310895e69ea65
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-27 13:33:26 +0900

    feat: 라우팅 세팅

--- oiso_frontend/src/App.js
+++ oiso_frontend/src/App.js
@@ -4,21 +4,16 @@ import Main from './Component/Main';
 import Login from './Component/Login';
 import RegistItem from './Component/RegistItem'
 import MyPage from './Component/MyPage/MyPage'
-
+import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div>
-      {/* // main page */}
-      {/* <Main />  */}
-
-      {/* // 로그인 페이지 */}
-      {/* <Login /> */}
-
-      {/* 상품 등록 페이지 */}
-      {/* <RegistItem /> */}
-
-      {/* 마이페이지 */}
-      <MyPage />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/registItem' element={<RegistItem />} />
+        <Route path='/myPage' element={<MyPage />} />
+      </Routes>
     </div>
   );
 }
